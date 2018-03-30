e2431ff279f426a4a6e78cce360ee177fc7b943f
yangbowen <yang0927mail@gmail.com>
2018-03-30 22:40:47 +0800

    跑步动画，持剑

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+
+    private PlayerController controller;
+
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+
+    void Update ()
+	{
+
+	    float horizontal = Input.GetAxis("Horizontal");
+	    float vertical = Input.GetAxis("Vertical");
+
+        controller.Horizontal = horizontal;
+        controller.Vertical = vertical;
+
+        controller.Tick(Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Horizontal;
+
+    public float Vertical;
+
+    private Animator ani;
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    public void Tick(float dt)
+    {
+        ani.SetFloat("vertical",Vertical);
+    }
+
+}
